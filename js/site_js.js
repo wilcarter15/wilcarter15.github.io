1a04ac20bdb64e8997ec31fef6ffbf400ac347f3
wilcarter15 <Will@Wills-MacBook-Pro.local>
2016-09-27 21:24:30 +0900

    removed aces files, added Leu 2.0 files

---
+++ js/site_js.js
@@ -0,0 +1,38 @@
+/*Custom Java for leubasketball.com */
+
+/*JS for email news letter popup*/
+
+     
+var alreadyDisplayed = localStorage.getItem('alreadyDisplayed');
+
+
+if (!alreadyDisplayed || alreadyDisplayed == 'false') {
+
+  setTimeout(function() {
+    $('#myModal').modal('show');
+
+    localStorage.setItem('alreadyDisplayed', 'true');
+
+  }, 3000); 
+}
+
+//home page scroll
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+
+    
+});
+ 
+
+
